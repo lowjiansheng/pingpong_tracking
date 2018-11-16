e013481ef98784cae50835aa37f1db35c3ea9c41
lowjiansheng <lowjiansheng@hotmail.sg>
2018-11-16 16:51:07 +0800

    ready for submission

--- feature_tracking_v2.m
+++ feature_tracking_v2.m
@@ -1,14 +1,12 @@
 % Record
 % Success
-% Angle 1: 1, 2, 3, 4, 5, 6, 7, 8, 9
-% Angle 2: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 (frame with woman shows greater
-% error, but still can track)
-% Angle 3: 1, 2, 4, 6 (stops tracking after ball flies off table), 3, 7, 8, 10
-% Problem
-% Angle 1: 10 (woman)
-% Angle 3: 5 (fast ball), 9 (woman)
-angle = 3;
-vid = 2;
+% Angle 1: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+% Angle 2: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+% Angle 3: 1, 2, 3, 4 ,5, 6, 7, 8, 9, 10
+
+% Input values to determine video file to be used.
+angle = 1;
+vid = 10;
 
 % Strings
 file_path_vid = "./TestVideos/";
@@ -44,7 +42,7 @@ while hasFrame(pingpong)
     k = k + 1;
 end
 num_frames = k - 1;
-
+    
 % extract foreground from every frame, change image into greyscale, store
 % the new matrix into matrix called foreground
 foreground = rgb2gray(mov(1).cdata - background.cdata);
@@ -54,55 +52,73 @@ end
 num_rows = size(foreground, 1);
 num_cols = size(foreground, 2);
 
-% Hard threshold by manual selection
-threshold_intensity_arr = [50, 50, 40];
+% Hard threshold by manual selection.
+threshold_intensity_arr = [50, 50, 37];
 threshold_intensity = threshold_intensity_arr(angle);
+threshold_rec = zeros(1, num_frames);
+% These values are used to eliminate noise from the calculated threshold
+% and allow for continued tracking even when intensity of pixels fall drastically.
+threshold_stability = [20 65 80];
 
 % Search space initialisation- 50 by 100 matrix
+% This is the window that we will be searching for the ball in.  
 int_row_low = [290 285 250];
 int_row_high = [340 335 300];
-int_col_low = [530 570 1265];
-int_col_high = [630 670 1365];
+int_col_low = [525 570 1255];
+int_col_high = [625 670 1355];
 
 row_low = int_row_low(angle);
 row_high = int_row_high(angle);
 col_low = int_col_low(angle);
 col_high = int_col_high(angle);
-
-% create array to store our coordinates of ball from feature tracker
-tracked_arr = zeros(num_frames, 3); 
 max_possible_num_of_pixels = 50 * 100;
 
+% Arrays that keep track of results for analysis. 
+tracked_arr = zeros(num_frames, 3); 
 num_no_ball = 0;
 num_with_ball = 0;
 
-% collate info of all pixels that will has an intensity >= threshold
+figure;
+% This is used to keep track of the previous displacement of the ball's
+% centroid. The value is used whenever we lose track of the ball midway to
+% ensure that window follows the ball. We assume that the ball will
+% continue in its direction of motion. 
+previous_displacements = zeros(1,2);
+
 for frame = 1:size(foreground, 3)
     tracked_arr(frame, 1) = frame - 1;
     x_coord = zeros(1, max_possible_num_of_pixels);
     y_coord = zeros(1, max_possible_num_of_pixels);
     above_count = 0;
-    
+    sum_above_threshold = 0;
+    % Collate info of all pixels that will has an intensity >= threshold
     for i = row_low:row_high
         for j = col_low:col_high
             i = min(i, num_rows);
             j = min(j, num_cols);
             if foreground(i,j,frame) >= threshold_intensity
+                sum_above_threshold = sum_above_threshold + double(foreground(i, j, frame));
                 above_count = above_count + 1;
                 x_coord(above_count) = i;
                 y_coord(above_count) = j;
             end 
         end
     end
-    
+    % There are no pixels in this window that are above the threshold. A
+    % ball is NOT detected. We will skip this frame and move on to the
+    % next.
     if above_count == 0
-        % no coordinates to fill in, go to next frame
         tracked_arr(frame, 2) = 0;
         tracked_arr(frame, 3) = 0;
         num_no_ball = num_no_ball + 1;
+        objectRegion = [col_low row_low col_high-col_low row_high-row_low];
+        framePrint = insertShape(foreground(:,:,frame), 'Rectangle', objectRegion, 'Color', 'red');
+        frame;
+
+        imshow(framePrint);
         continue;
     end
-    % if we've reached here it means we've found the ball
+    % Note: If we've reached here it means we've found the ball.
     num_with_ball = num_with_ball + 1;
     x_coord_sum = 0;
     y_coord_sum = 0;
@@ -112,24 +128,41 @@ for frame = 1:size(foreground, 3)
     end
     x_centroid_pos = ceil(x_coord_sum / above_count);
     y_centroid_pos = ceil(y_coord_sum / above_count);
-    % write x_centroid_pos and y_centroid_pos to the array
-    % there are some bugs with swapping rows and columns
+    % Storing the x_centroid_pos and y_centroid_pos to tracked_arr for
+    % debugging and analysis.
     tracked_arr(frame, 2) = y_centroid_pos;
     tracked_arr(frame, 3) = x_centroid_pos;
     
-    % update displacement if necessary
+    % Moving the window.
     if num_with_ball >= 2
-        x_displacement = x_centroid_pos - tracked_arr(frame-1, 3);
-        y_displacement = (y_centroid_pos - tracked_arr(frame-1, 2));
-        row_low = row_low + x_displacement;
-        row_low = max(row_low, 1);
-        row_high = row_high + x_displacement;
-        row_high = max(row_high, 1);
-        col_low = col_low + y_displacement;
-        col_low = max(col_low, 1);
-        col_high = col_high + y_displacement;
-        col_high = max(col_high, 1);
+        if tracked_arr(frame-1,3) == 0 || tracked_arr(frame-1,2) == 0
+            row_low = row_low + previous_displacements(1) + 20;
+            col_low = col_low + previous_displacements(2) + 5;
+            row_high = row_high + previous_displacements(1) + 20;
+            col_high = col_high + previous_displacements(2) + 5;
+        else
+            x_displacement = x_centroid_pos - tracked_arr(frame-1, 3);
+            y_displacement = (y_centroid_pos - tracked_arr(frame-1, 2));
+            row_low = row_low + x_displacement;
+            row_low = max(row_low, 1);
+            row_high = row_high + x_displacement;
+            row_high = max(row_high, 1);
+            col_low = col_low + y_displacement;
+            col_low = max(col_low, 1);
+            col_high = col_high + y_displacement ;
+            col_high = max(col_high, 1); 
+            previous_displacements = [x_displacement y_displacement];
+        end
     end
+    % Altering the threshold for the next frame. 
+    threshold_intensity = max((sum_above_threshold / above_count) - threshold_stability(angle), threshold_intensity_arr(angle));
+    threshold_rec(frame) = threshold_intensity;
+    objectRegion = [col_low row_low col_high-col_low row_high-row_low];
+    % Visualising the position of the ball in the frame. 
+    frame;
+    framePrint = insertMarker(mov(frame).cdata, [y_centroid_pos x_centroid_pos], '+', 'Color', 'red');
+    framePrint = insertShape(framePrint, 'Rectangle', objectRegion, 'Color', 'red');
+    imshow(framePrint);
 end
 
 % Values checking
@@ -155,7 +188,7 @@ for frame = 1:size(annotated_csv,1)
         sum_euclidean_dist = sum_euclidean_dist + diff;
         num_correct_frames_marked = num_correct_frames_marked + 1;
     end
-end
+ end
 
 disp(num_wrong_ball);
 disp(num_missed_ball);
